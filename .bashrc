66ff618db3f8270e8430de5bb930f78a7043dcbf
Eliphaz Bouye <eliphazbouye@gmail.com>
2024-06-04 11:23:41 +0000

    Update bashrc and add lsp, company and flycheck on init.el emacs

--- .bashrc
+++ .bashrc
@@ -9,10 +9,13 @@ alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 PS1='[\u@\h \W]\$ '
 
-alias sc="/usr/bin/git --git-dir=$HOME/.sopho-config/ --work-tree=$HOME"
+alias dpz="/usr/bin/git --git-dir=$HOME/.phaz-config/ --work-tree=$HOME"
 export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
 
-alias spm='sc push -u origin main'
-alias sf='sc fetch'
-alias spull='sc pull'
-alias scc='sc commit -m'
+alias dpm='dpz push -u origin main'
+alias dpzf='dpz fetch'
+alias dpzpull='dpz pull'
+alias dpzcc='dpz commit -m'
+
+# NodeJS
+export PATH=$HOME/Programming/nodejs/node-v22.2.0-linux-x64/bin:$PATH
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,6 +12,9 @@
 
 (add-hook 'prog-mode-hook 'line-numbers-and-relative)
 
+;; Manage backup files
+(setq backup-directory-alist '(("." . "~/.emacs-backup-file")))
+
 ;; Remove some visual unless
 (menu-bar-mode -1)
 (tool-bar-mode -1)
@@ -41,10 +44,46 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(zenburn-theme)))
+ '(package-selected-packages
+   '(typescript-mode company flycheck lsp-pyright lsp-mode zenburn-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; FlyCheck
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+(use-package company
+  :ensure t
+  :init (global-company-mode)
+  :config
+  (setq company-minimum-prefix-length 2))
+
+;; Typescript mode
+(use-package typescript-mode
+  :ensure t)
+
+;; LSP
+(use-package lsp-mode
+  :ensure t
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         ((javascript-mode typescript-mode python-mode php-mode c-mode) . lsp-deferred)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp-deferred)
+
+
+;; Python LSP
+(use-package lsp-pyright
+  :ensure t
+  :hook (python-mode . (lambda ()
+                         (require 'lsp-pyright)
+                         (lsp-deferred))))  ; or lsp-deferred
